2498a0717a072ff1218eb0e158b4513a7e8fc590
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-03-23 20:45:21 +0000

    Create منشن2.js

---
+++ plugins/منشن2.js
@@ -0,0 +1,25 @@
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+    if (isAdmin || isOwner) {
+        let pesan = args.join` `;
+        let oi = `*♚~الرساله:* ${pesan}`;
+        let teks = `*  * \n\n ${oi}\n\n*🗡┇الجروب :⇣*\n`;
+        for (let mem of participants) {
+            if (!mem.isAdmin) { // تحقق مما إذا كان العضو ليس مشرفًا
+                teks += `*تفاعل ↫* @${mem.id.split('@')[0]}\n`;
+            }
+        }
+        teks += `*shanks❤️💍*\n\n*miral❤️💍*`;
+        conn.sendMessage(m.chat, { text: teks, mentions: participants.filter(mem => !mem.isAdmin).map(a => a.id) });
+    } else {
+        global.dfail('admin', m, conn);
+        throw false;
+    }
+}
+
+handler.help = ['tagall <mesaje>','invocar <mesaje>'];
+handler.tags = ['group'];
+handler.command = /^(2منشن|invocar|invocacion|todos|invocación)$/i;
+handler.admin = true;
+handler.group = true;
+
+export default handler;
