8f9abcd5630382c58035545f4a8eb87d54c6c23b
Tony stark <142097798+Kyutaka101@users.noreply.github.com>
2024-03-23 21:40:17 +0000

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,17 +1,17 @@
-let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
-    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`${text ? `${text}
-*✦━━─━━━⌠🐦⌡━━━─━━✦*
-*⤹⚜ المنشن ⊰⚡⊱ الجماعي ⚜⤸*
-\n` + users.map(v => '💎│◦❈↲ @' + v.replace(/@.+/, '')).join`\n` + '\n*✦━━━━━━[  بوت ]━━━━━━✦*', null, {
-        mentions: users
-    })
+let handler = async (m, { conn, text, participants, groupMetadata }) => {
+  let users = participants.filter(p => !p.isAdmin).map(u => u.id)
+  
+  m.reply(`❏ مجموعة : *${groupMetadata.subject}*\n❏ أعضاء : *${participants.length}*${text ? `\n❐ رسالة : ${text}\n` : ''}\n*✦━━━━━━[ 𝑍𝑂𝑅𝑂 ]━━━━━━✦*
+*⤹⚜⊰⚡⊱⚜⤸* يلا ي جدعان تعالو اي الملل دا
+*✦━━─━━━⌠🐉⌡━━━─━━✦*
+*⤹⚜ المنشن ⊰⚡⊱ الجماعي ⚜⤸*\n` + users.map(v => '│◦❈↲ تفاعل @' + v.replace(/@.+/, '')).join`\n` + '\n*✦━━━━━━[ 𝑍𝑂𝑅𝑂 ]━━━━━━✦*', null, {
+      mentions: users
+  })
 }
 
-handler.help = ['منشن']
+handler.help = ['tagall']
 handler.tags = ['group']
 handler.command = ['منشن']
-handler.admin = true
 handler.group = true
-handler.botAdmin = true
+
 export default handler
